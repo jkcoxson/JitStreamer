3aa677468510532ed5571cc5fdbed468199ec982
Jackson Coxson <admin@jkcoxson.com>
2022-04-17 20:44:38 -0600

    Fix building on arm Linux

---
+++ buildenv/release.sh
@@ -0,0 +1,7 @@
+docker build --build-arg IMAGE=arm32v7/alpine:3.15 --output . .
+
+# docker build --build-arg IMAGE=arm64v8/alpine:3.15 .
+
+# docker build --build-arg IMAGE=amd64/alpine:3.15 .
+
+# docker build --build-arg IMAGE=i386/alpine:3.15 .
